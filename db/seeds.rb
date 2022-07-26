b1ebff34e4ef1f479cc3c802e4ce193f07381b61
VirginiaGomezR <virginiagomezr@gmail.com>
2022-07-26 14:30:48 +0200

    Depot Series Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(title: '',
+description:
+%{<p>
+Ranma ½ es una serie de manga escrita e ilustrada por Rumiko Takahashi. 
+Se publicó originalmente en la revista Shūkan Shōnen Sunday de la editorial Shōga
+kukan desde el 19 de agosto de 1987 hasta el 6 de marzo de 1996, organizándose
+en 38 volúmenes en formato tankōbon
+</p>}
+)

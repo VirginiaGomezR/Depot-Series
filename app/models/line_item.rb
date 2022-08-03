d98d07a15ba64e29a5c0fb39111d2c54ad37f192
VirginiaGomezR <virginiagomezr@gmail.com>
2022-08-03 14:16:44 +0200

    smarter card added

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,11 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+  after_initialize do
+    self.quantity ||= 1
+  end
+  def total_price
+    product.price * quantity
+  end
 end
 

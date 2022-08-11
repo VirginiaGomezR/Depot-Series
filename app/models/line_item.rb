dd2b73d8b29ac029fdf59574ea5bf5bb7937b7a9
VirginiaGomezR <virginiagomezr@gmail.com>
2022-08-11 13:47:17 +0200

    Capturing an order added

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ApplicationRecord
+  belongs_to :order, optional: true
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
   after_initialize do
     self.quantity ||= 1
   end

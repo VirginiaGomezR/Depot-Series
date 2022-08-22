784e5c9e3e609b2fb958cef0447c36e8a0df34fa
VirginiaGomezR <virginiagomezr@gmail.com>
2022-08-22 09:44:59 +0200

    pay-type dropdown translation added

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,16 @@ module Depot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.filter_parameters += [ :credit_card_number]
+    config.middleware.use I18n::JS::Middleware
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # Permitted locales available for the application
+    #config.I18n.available_locales = [:en, :es]
+
+    # Set default locale to something other than :en
+    #config.I18n.default_locale = :es
+
   end
 end
